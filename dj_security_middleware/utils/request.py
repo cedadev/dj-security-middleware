0871c135700e481396abb79e66fdb215345bb9d5
wtucker <william.tucker@stfc.ac.uk>
2018-10-26 15:01:50 +0000

    Added util functions for getting login and logout URLs. Simplified middleware class

--- dj_security_middleware/utils/request.py
+++ dj_security_middleware/utils/request.py
@@ -13,12 +13,19 @@ __contact__ = "william.tucker@stfc.ac.uk"
 import logging
 
 from django.conf import settings
+from django.utils.http import urlencode
 
 from dj_security_middleware.exception import MissingCookieError
 from .cookie import parse_cookie_value
 
 
 log = logging.getLogger(__name__)
+
+DEFAULT_REDIRECT_KEY = 'r'
+
+# URL query parameter keys recognized by middleware.
+LOGOUT_KEY = 'logout'
+REGISTRATION_KEY = 'registration'
 
 
 def userid_from_request(request):
@@ -56,3 +63,45 @@ def openid_from_request(request):
         
     else:
         log.warn("Can't get OpenID without OPENID_COOKIE_NAME setting.")
+
+
+def login_url(request, redirect=True):
+    """Construct a login URL pointing to the configured login service.
+    This will contain the current absolute URI of the request as an
+    encoded query string parameter which the login service should
+    return to after authentication.
+    
+    :param request: The HTTP request object.
+    :param redirect_key: The key used for the redirect parameter.
+    """
+    
+    login_service = settings.SECURITY_LOGIN_SERVICE
+    if redirect:
+        redirect_key = getattr(
+            settings, 'REDIRECT_FIELD_NAME', DEFAULT_REDIRECT_KEY)
+        
+        query = {redirect_key: request.build_absolute_uri()}
+        return '{login_service}?{query}'.format(
+            login_service=login_service,
+            query=urlencode(query)
+        )
+        
+    else:
+        return login_service
+
+
+def logout_url(request):
+    """Returns the full path of the request with an additional logout
+    parameter added to the query string. This can be intercepted by
+    dj-security-middleware to perform a logout.
+    
+    :param request: The HTTP request object.
+    :returns: A logout URL path.
+    """
+    
+    query = request.GET.copy()
+    query[LOGOUT_KEY] = None
+    
+    logout_path = '{path}?{query}'.format(
+        path=request.path, query=urlencode(query))
+    return logout_path
